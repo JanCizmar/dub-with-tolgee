e7eadd3a119db33970615d7f3e63a2bdfcfeeb80
David Asaolu <67129211+dha-stix@users.noreply.github.com>
2024-10-23 09:23:09 +0100

    fix: Added Tolgee to the Dashboard layout.tsx file

--- apps/web/app/app.dub.co/(dashboard)/layout.tsx
+++ apps/web/app/app.dub.co/(dashboard)/layout.tsx
@@ -4,18 +4,26 @@ import Toolbar from "@/ui/layout/toolbar/toolbar";
 import { constructMetadata } from "@dub/utils";
 import { ReactNode } from "react";
 import Providers from "../../providers";
+import { getStaticData } from "../../../tolgee/shared.ts";
+import {TolgeeNextProvider} from "../../../tolgee/client.tsx";
+import { getUserLocale } from "../../../tolgee/locale.ts";
 
 export const dynamic = "force-static";
 export const metadata = constructMetadata();
 
 export default async function Layout({ children }: { children: ReactNode }) {
+  const locale = getUserLocale();
+  const locales = await getStaticData([locale, "en"]);
+
   return (
+    <TolgeeNextProvider locale={locale} locales={locales}>
     <Providers>
       <div className="min-h-screen w-full bg-white">
         <MainNav toolContent={<HelpButtonRSC />}>{children}</MainNav>
       </div>
       {/* <ChangelogPopup /> */}
       <Toolbar show={["onboarding"]} />
-    </Providers>
+      </Providers>
+    </TolgeeNextProvider>
   );
 }
